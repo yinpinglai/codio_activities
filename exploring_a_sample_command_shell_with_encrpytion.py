a37277aaf708e6d5c3203927820d8af8626a20c2
Yin Ping, Lai <ypinglai29@gmail.com>
2022-05-10 20:44:01 +0800

    added: the shell command along with the encryption method

--- exploring_a_sample_command_shell_with_encrpytion.py
+++ exploring_a_sample_command_shell_with_encrpytion.py
@@ -1,6 +1,11 @@
-import os
 from cmd import Cmd
+
+import os
+import base64
+import uuid
 from cryptography.fernet import Fernet
+from cryptography.hazmat.primitives import hashes
+from cryptography.hazmat.primitives.kdf.pbkdf2 import PBKDF2HMAC
 
 class MyPrompt(Cmd):
 
@@ -8,15 +13,63 @@ class MyPrompt(Cmd):
   intro = "Welcome! Type ? to list commands"
 
   def __init__(self) -> None:
-    self.key = Fernet.generate_key()
-    self.cipher_suite = Fernet(self.key)
     super().__init__()
 
-  def do_encrypt(self, plain_text):
-    return self.cipher_suite.encrypt(plain_text)
+  def _generate_salt(self):
+    salt_file_name = '.salt'
+    if not os.path.exists(salt_file_name):
+      with open(salt_file_name, 'wb') as salt_file:
+        salt_file.write(uuid.uuid4().hex)
+    with open(salt_file_name, 'rb') as salt_file:
+      return salt_file.read()
 
-  def do_decrypt(self, chiper_text):
-    return self.cipher_suite.decrypt(chiper_text)
+  def do_encrypt(self, inp):
+    input_password = inp[0]
+    input_data = inp[1]
+    export_data_file = inp[2]
+
+    password = input_password.encode('utf-8')
+    salt = self._generate_salt()
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
+                     length=32,
+                     salt=salt,
+                     iterations=310000)
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    fernet = Fernet(key)
+
+    try:
+      with open(export_data_file, 'rb') as export_file:
+        encrypted_data = fernet.encrypt(input_data)
+        export_data_file.write(encrypted_data)
+      print('Saved to {}'.format(export_data_file))
+    except Exception as exception:
+      print(exception)
+
+  def help_encrypt(self):
+    print('help_encrypt')
+
+  def do_decrypt(self, inp):
+    input_password = inp[0]
+    encrypted_file_path = inp[1]
+  
+    password = input_password.encode('utf-8')
+    salt = self._generate_salt()
+    kdf = PBKDF2HMAC(algorithm=hashes.SHA256(),
+                     length=32,
+                     salt=salt,
+                     iterations=310000)
+    key = base64.urlsafe_b64encode(kdf.derive(password))
+    fernet = Fernet(key)
+    try:
+      with open(encrypted_file_path, 'rb') as input_file:
+        data = input_file.read()
+        decrypted_data = fernet.decrypt(data)
+        print(str(decrypted_data))
+    except Exception as exception:
+      print(exception)
+
+  def help_decrypt(self):
+    print('help_decrypt')
 
   def do_exit(self, inp):
     print("Bye")
@@ -61,18 +114,16 @@ class MyPrompt(Cmd):
         for dir in d:
           # print all sub folders inside the directory
           dir_path = os.path.join(r, dir)
-          encrypted_dir_path = self.do_encrypt(dir_path.encode())
-          print(str(encrypted_dir_path))
+          print(dir_path)
 
         for file in f:
           # print all files inside the directory
           file_path = os.path.join(r, file)
-          encrypted_file_path = self.do_encrypt(file_path.encode())
-          print(str(encrypted_file_path))
+          print(file_path)
 
     except Exception:
       print("list: no such file or directory: {}".format())
-
+    
   def help_list(self):
     print("List the content for the current direcotry.")
 
